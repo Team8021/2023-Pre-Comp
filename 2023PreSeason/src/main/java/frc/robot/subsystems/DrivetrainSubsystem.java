a5cf7c90e185f27053b1e8760dac09ad29256b81
Mateuuu <80788450+Mateuuu@users.noreply.github.com>
2022-03-22 11:25:30 -0700

    Simulations

--- 2023PreSeason/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ 2023PreSeason/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -4,12 +4,30 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DrivetrainSubsystem extends SubsystemBase {
-  public DrivetrainSubsystem() {}
+  public DrivetrainSubsystem() {
+    SmartDashboard.putData("Field", m_field);
+    leftEncoder.reset();
+    rightEncoder.reset();
+  }
 
   private CANSparkMax m_leftMotor = new CANSparkMax(Constants.MOTOR_LEFT_ID, MotorType.kBrushless);
   private CANSparkMax m_rightMotor = new CANSparkMax(Constants.MOTOR_RIGHT_ID, MotorType.kBrushless);
@@ -57,6 +75,45 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    m_odometry.update(m_gyro.getRotation2d(),
+                      leftEncoder.getDistance(),
+                      rightEncoder.getDistance());
+    m_field.setRobotPose(m_odometry.getPoseMeters());
+  }
+
+
+
+
+  private Encoder leftEncoder = new Encoder(0, 1);
+  private Encoder rightEncoder = new Encoder(2, 3);
+  private EncoderSim m_leftEncoderSim = new EncoderSim(leftEncoder);
+  private EncoderSim m_rightEncoderSim = new EncoderSim(rightEncoder);
+  private AnalogGyro m_gyro = new AnalogGyro(1);
+  private AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
+  private Field2d m_field = new Field2d();
+  private DifferentialDrivetrainSim m_DifferentialDrivetrainSim = new DifferentialDrivetrainSim(
+    DCMotor.getNEO(2),
+    7.29,
+    7.5,
+    60,
+    Units.inchesToMeters(3),
+    .7112,
+    VecBuilder.fill(.001, .001, .001, .1, .1, .005, .005)
+    );
+    private DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d(), new Pose2d(5, 13.5, new Rotation2d()));
     
+  @Override
+  public void simulationPeriodic()
+  {
+    m_DifferentialDrivetrainSim.setInputs(m_leftMotor.get() * RobotController.getInputVoltage(), 
+                                          m_rightMotor.get() * RobotController.getInputVoltage());
+    m_DifferentialDrivetrainSim.update(0.02);
+
+
+    m_leftEncoderSim.setDistance(m_DifferentialDrivetrainSim.getLeftPositionMeters());
+    m_leftEncoderSim.setRate(m_DifferentialDrivetrainSim.getLeftVelocityMetersPerSecond());
+    m_rightEncoderSim.setDistance(m_DifferentialDrivetrainSim.getRightPositionMeters());
+    m_rightEncoderSim.setRate(m_DifferentialDrivetrainSim.getRightVelocityMetersPerSecond());
+    m_gyroSim.setAngle(-m_DifferentialDrivetrainSim.getHeading().getDegrees());
   }
 }
